209f19f4ae40344df9005560e44322fea0424985
JuanJoven01 <juanjov95@hotmail.com>
2025-01-13 20:38:35 +0100

    feat(controller): validation code
    Code now is generated on the controller, tested, and return error when email is repeated

--- controllers/users_handler.py
+++ controllers/users_handler.py
@@ -1,5 +1,30 @@
 from odoo import http
 from odoo.http import request
+import random, string
+def _generate_code(user_id, length=6):
+        """
+        Generate a random alphanumeric code of the given length and saves that on table
+        """
+        code = ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
+        user = request.env['easy_user.activation_code'].sudo().create({
+             'user_id' : user_id,
+             'activation_code' : code
+        }) 
+        print('is here **************************************************')
+        print(user.read())
+        return user.read()[0]['activation_code']
+
+def _verify_code(code, user_id):
+        """
+        Verify the activation code and activate the user if correct.
+        """
+        user = request.env['easy_user.activation_code'].sudo().search([('user_id', '=', user_id)], limit=1)
+        return True
+
+def _send_email(activation_code):
+    print('activation_code ########################')
+    print(activation_code)
+    return True
 class EasyUsers(http.Controller):
     @http.route('/api/easy_apps/users/new_user', methods=['POST'], type='jsonrpc', auth='public')
     def create_easy_user(self, **kwargs):
@@ -9,7 +34,7 @@ class EasyUsers(http.Controller):
         try:
             # get the requested data
             email = kwargs.get('email')
-            name = kwargs.get('user_name')
+            name = kwargs.get('name')
             password = kwargs.get('password')
 
             if not email or not name or not password:
@@ -18,11 +43,15 @@ class EasyUsers(http.Controller):
                 'message': 'Fields email, name and password are required'
                 }
             #verify if email already exist
-            user = request.env['res.users'].sudo().search([('login', '=', email)], limit=1)
+            user = request.env['res.users'].sudo().search([('login', '=', email), '|', ('active', '=', True), ('active', '=', False)], limit=1)
+            print('email ##################')
+            print(email)
+            print('is here 2222222222**************************************************')
+            print(user.read())
             if user:
                 return {
                 'error': 'Repeated email',
-                'message': 'The Email is already registred'
+                'message': 'The Email is already registered'
                 }
             # Create the user
             new_user = request.env['res.users'].sudo().create({
@@ -33,14 +62,16 @@ class EasyUsers(http.Controller):
                 'groups_id': [(4, 12)],  # Add the user at the group ID 12 (easy_apps), the code 4 ensure do not remove any register
             })
 
+            code = _generate_code(new_user.id)
+            _send_email(code)
             return {
                 'success': True,
-                'message': 'Usuario creado exitosamente',
+                'message': 'User created successful',
                 'user_id': new_user.id,
             }
         except Exception as e:
             return {
                 'error': str(e),
-                'message': 'Hubo un problema al crear el usuario'
+                'message': 'Error creating the user'
             }
         oo
--- models/models.py
+++ models/models.py
@@ -4,28 +4,8 @@ import string
 
 
 class EasyUserActivationCode(models.Model):
-    _name = 'easy.user.activation.code'
+    _name = 'easy_user.activation_code'
     _description = 'User Activation Code'
 
     user_id = fields.Many2one('res.users', string='User', required=True, ondelete='cascade', unique=True)
     activation_code = fields.Char(string='Activation Code', required=True, default=lambda self: self._generate_code())
-
-    @staticmethod
-    def _generate_code(length=6):
-        """
-        Generate a random alphanumeric code of the given length.
-        """
-        return ''.join(random.choices(string.ascii_uppercase + string.digits, k=length))
-
-    def _verify_code(self, code):
-        """
-        Verify the activation code and activate the user if correct.
-        """
-        if self.activation_code != code:
-            return False
-
-        # Activate the user
-        self.user_id.active = True
-        # Optionally, delete the activation record after verification
-        self.unlink()
-        return True
